d9a1d9905486a4c3c6c89775312c18d400740d90
jwt-123 <56295743+jwt-123@users.noreply.github.com>
2020-11-18 14:51:08 +0800

    Update and rename SinglyLinkedList.cpp to dataStructure/SinglyLinkedList.cpp

--- dataStructure/SinglyLinkedList.cpp
+++ dataStructure/SinglyLinkedList.cpp
@@ -2,10 +2,11 @@
 #include<malloc.h>
 typedef int Datatype;
 using namespace std;
+
 typedef struct linknode
 {
     int data;
-    struct linknode *next;
+    struct linknode* next;
 }Linknode;
 
 Linknode* InitList()//初始化
@@ -22,20 +23,22 @@ void CreateListH(Linknode* head, int n)//头插法
     cout << "plz input", n, "number";
     for (int i = 0; i < n; i++)
     {
+        s = (linknode*)malloc(sizeof(linknode));
         cin >> s->data;
         s->next = head->next;
         head->next = s;
     }
-    cout << "succeed build Linklist";
+    cout << "succeed build Linklist"<<endl;
 }
 
 void CreateListL(Linknode* head, int n)
 {
-    Linknode* s, *last;
+    Linknode* s, * last;
     last = head;
     cout << "plz input", n, "number";
     for (int i = 0; i < n; i++)
     {
+        s = (linknode*)malloc(sizeof(linknode));
         cin >> s->data;
         s->next = NULL;
         last->next = s;
@@ -44,11 +47,11 @@ void CreateListL(Linknode* head, int n)
     cout << "succeed built Linklist";
 }
 
-int lengthlist(Linknode *head)
+int lengthlist(Linknode* head)
 {
     Linknode* p = head->next;
     int i = 0;
-    while (p!=NULL)
+    while (p != NULL)
     {
         p = p->next;
         i++;
@@ -61,19 +64,19 @@ void locate(linknode* head, Datatype x)
     int i = 1;
     linknode* p;
     p = head->next;
-    while (p!=NULL&&p->data!=x)
+    while (p != NULL && p->data != x)
     {
         p = p->next;
         i++;
     }
 
-    if (p!=NULL)
+    if (p != NULL)
     {
-        cout << "locate",i,"find the",x;
+        cout << "locate", i, "find the", x;
     }
     else
     {
-        cout << "fail to find";
+        cout << "fail to find"<<endl;
     }
 }
 
@@ -81,10 +84,10 @@ void searchlist(linknode* head, int i)
 {
     linknode* p;
     int j = 0;
-    p = head; 
-    if (i>lengthlist(head))
+    p = head;
+    if (i > lengthlist(head))
     {
-        while (p->next!=NULL&&j<i)
+        while (p->next != NULL && j < i)
         {
             p = p->next;
             j++;
@@ -101,22 +104,22 @@ void Inslist(linknode* head, int i, Datatype x)
     int j = 0;
     linknode* p, * s;
     p = head;
-    while (p->next!=NULL && j<i-1)
+    while (p->next != NULL && j < i - 1)
     {
         p = p->next;
         j++;
     }
-    if (p!=NULL)
+    if (p != NULL)
     {
         s = (linknode*)malloc(sizeof(linknode));
         s->data = x;
-        s ->next = p->next;
+        s->next = p->next;
         p->next = s;
-        cout << "succeed to insert";
+        cout << "succeed to insert"<<endl;
     }
     else
     {
-        cout << "fail to insert";
+        cout << "fail to insert" <<endl;
     }
 }
 
@@ -136,11 +139,11 @@ void delllist(linknode* head, int i)
         x = s->data;
         p->next = s->next;
         free(s);
-        cout << "dell",x,"at the", i;
+        cout << "dell", x, "at the", i;
     }
     else
     {
-        cout << "fail to dell,plz try again";
+        cout << "fail to dell,plz try again" << endl;
     }
 }
 
@@ -148,25 +151,25 @@ void delllist(linknode* head, int i)
 void displist(linknode* head)
 {
     linknode* p;
-    p = head -> next;
+    p = head->next;
     while (p != NULL)
     {
         cout << p->data;
-        p = p -> next;
+        p = p->next;
     }
 }
 
 void menuline()
 {
-    cout << "线性表子系统" << endl;
+    cout << "LinearMeterSubsystem" << endl;
     cout << "===================" << endl;
-    cout << "1,建立" << endl;
-    cout << "2,插入" << endl;
-    cout << "3,按位置查找" << endl;
-    cout << "4,按元素查找" << endl;
-    cout << "5，求表长" << endl;
-    cout << "6,返回" << endl;
-    cout << "请输入菜单号" << endl;
+    cout << "1,set up" << endl;
+    cout << "2,insert" << endl;
+    cout << "3,search in position" << endl;
+    cout << "4,search in value" << endl;
+    cout << "5，cal length" << endl;
+    cout << "6, return" << endl;
+    cout << "plz input the number" << endl;
 }
 
 
@@ -175,9 +178,9 @@ int main()
     linknode* head;
     Datatype x;
     int i, n;
-    char ch1, ch2,a;
+    char ch1, ch2, a;
     ch1 = 'y';
-    while (ch1=='y'||ch1=='Y')
+    while (ch1 == 'y' || ch1 == 'Y')
     {
         menuline();
         cin >> ch2;
@@ -196,9 +199,45 @@ int main()
             cin >> i;
             getchar();
             cout << "plz input the value" << endl;
-            // tomorrow will be better!
-        default:
+        case '3':
+            cout << "plz input the location";
+            cin >> i;
+            delllist(head, i);
+            cout << "the menu is" << endl;
+            displist(head);
             break;
+        case '4':
+            cout << "plz input the value you wanna search";
+            cin >> i;
+            searchlist(head, i);
+            break;
+        case '5':
+            cout << "plz input the integer you search";
+            cin >> x;
+            locate(head, x);
+            break;
+        case '6':
+            cout << "the length is";
+            cout << lengthlist(head);
+            break;
+        case '0':
+            ch1 = 'n';
+            break;
+        default:
+            cout << "error"<<endl;
+            break;
+        }
+        if (ch2!='0')
+        {
+            cout << "\nerror " << endl;
+            a = getchar();
+            if (a!='\xA')
+            {
+                getchar();
+                ch1 = 'n';
+
+            }
+
         }
     }
 }
